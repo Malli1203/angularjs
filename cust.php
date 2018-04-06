8d7621570bf504e7f889105ee231cf0780bb8b53
developer <36160058+Malli1203@users.noreply.github.com>
2018-04-05 21:24:52 -0700

    Add files via upload

---
+++ cust.php
@@ -0,0 +1,20 @@
+
+
+{ "records":[ 
+{"Name":"Alfreds Futterkiste","Country":"Germany"}, 
+{"Name":"Ana Trujillo Emparedados y helados","Country":"Mexico"}, 
+{"Name":"Antonio Moreno Taquería","Country":"Mexico"}, 
+{"Name":"Around the Horn","Country":"UK"}, 
+{"Name":"B's Beverages","Country":"UK"}, 
+{"Name":"Berglunds snabbköp",Country":"Sweden"}, 
+{"Name":"Blauer See Delikatessen","Country":"Germany"}, 
+{"Name":"Blondel père et fils","Country":"France"},
+ {"Name":"Bólido Comidas preparadas","Country":"Spain"}, 
+ {"Name":"Bon app","Country":"France"}, 
+ {"Name":"Bottom-Dollar Marketse","Country":"Canada"}, 
+ {"Name":"Cactus Comidas para llevar","Country":"Argentina"}, 
+ {"Name":"Centro comercial Moctezuma","Country":"Mexico"}, 
+ {"Name":"Chop-suey Chinese","Country":"Switzerland"}, 
+ {"Name":"Comércio Mineiro","Country":"Brazil"} 
+ ] }
+ 
---
+++ customers.php
@@ -0,0 +1,13 @@
+
+{"records" :[
+	{ "Name" : "aaaa","Country" : "India0"},
+	{ "Name" : "bbbb","Country" : "India1"},
+	{ "Name" : "cccc","Country" : "India2"},
+	{ "Name" : "dddd","Country" : "India3"},
+	{ "Name" : "eeee","Country" : "India4"},
+	{ "Name" : "ffff","Country" : "India5"},
+	{ "Name" : "gggg","Country" : "India6"},
+	{ "Name" : "hhhh","Country" : "India7"},
+	{ "Name" : "iiii","Country" : "India8"},
+	{ "Name" : "kkkk","Country" : "India9"},
+]}
